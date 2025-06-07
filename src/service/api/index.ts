0a91831e4b37fb9d8211ca3fa70057d97bb6091b
Barchinoy0803 <barchinoyyusupova33@gmail.com>
2025-06-07 12:13:01 +0500

    task 6: url updated

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const mainApi = createApi({
     reducerPath: "mainApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:3000",
+        baseUrl: "https://presentation-project-server.onrender.com",
         prepareHeaders: (headers) => {
             return headers;
         },
